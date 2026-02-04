13b693d7fd947be12385f997b4a904a24527e7d0
SatyabrataBhattacharjee <satyabratbhattacharya1408@gmail.com>
2026-02-04 17:43:11 +0530

    Update init_db.py

--- src/ingestion/init_db.py
+++ src/ingestion/init_db.py
@@ -4,19 +4,36 @@ from src.ingestion.db import engine
 def init_database():
     with engine.begin() as conn:
 
+        # -----------------------------
+        # CUSTOMER 7-DAY SUMMARY TABLE
+        # -----------------------------
         conn.execute(text("""
-            CREATE TABLE IF NOT EXISTS housing_data (
-                id SERIAL PRIMARY KEY,
-                size INTEGER,
-                bedrooms INTEGER,
-                age INTEGER,
-                location_score FLOAT,
-                income_index FLOAT,
-                price FLOAT,
-                created_at TIMESTAMP DEFAULT NOW()
+            CREATE TABLE IF NOT EXISTS customer_7day_summary (
+         
+
+                -- Numerical (mean aggregated)
+                quantity FLOAT CHECK (quantity >= 0),
+                line_net_amount FLOAT CHECK (line_net_amount >= 0),
+                total_items FLOAT CHECK (total_items >= 0),
+                total_cost FLOAT CHECK (total_cost >= 0),
+
+                -- Categorical (mode aggregated)
+               
+                
+                loyalty_status TEXT,
+             
+               
+                
+                payment_method TEXT,
+                discount_applied BOOLEAN,
+
+
             );
         """))
 
+        # -----------------------------
+        # PIPELINE STATE TABLE
+        # -----------------------------
         conn.execute(text("""
             CREATE TABLE IF NOT EXISTS pipeline_state (
                 key TEXT PRIMARY KEY,
